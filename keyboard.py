e17c2a560d0c7fbda29a64b9dda8f9e8910407b0
Jon Coffey <jkc5928@psu.edu>
2021-11-27 22:10:36 -0800

    Initial keyboard mode with no buzzer

--- keyboard.py
+++ keyboard.py
@@ -26,36 +26,45 @@ class Keyboard:
 
     @staticmethod
     def play(buzzer):
-        returnSequence = ['G5', 'G5', 'G5', 'G5', 'G5']
+        returnSequence = ['C5', 'C5', 'C5', 'C5', 'C5']
         keyReturnList = []
         #initialise a previous input variable to 0 (Assume no pressure applied)
-        prev_input = 0
-        try:
-            while True:
-                if (len(keyReturnList) == 5 and keyReturnList == returnSequence):
-                        break;
-                #take a reading
-                KEY1 = GPIO.input(4)
-                KEY2 = GPIO.input(14)
-                KEY3 = GPIO.input(15)
-                KEY4 = GPIO.input(17)
-                KEY5 = GPIO.input(18)
-                #if the last reading was low and this one high, alert us
-                if ((not prev_input) and KEY1):
-                    print("under pressure")
-               # if ((not prev_input) and KEY2):
-               #     print(KEY2)
-               # if ((not prev_input) and KEY3):
-               #     print(KEY3)
-               # if ((not prev_input) and KEY4):
-               #     print(KEY4)
-               # if ((not prev_input) and KEY5):
-               #     print(KEY5)
-                #update previous input
-                prev_input = KEY1
-                #slight pause
-                time.sleep(0.10)
-        except KeyboardInterrupt:
-            pass
-        finally:
-            GPIO.cleanup()
+        prev_KEY1 = 0
+        prev_KEY2 = 0
+        prev_KEY3 = 0
+        prev_KEY4 = 0
+        prev_KEY5 = 0
+        while True:
+            print(keyReturnList)
+            if (len(keyReturnList) == 5):
+                print(keyReturnList)
+                if (keyReturnList == returnSequence):
+                    break;
+            if (len(keyReturnList) > 5):
+                keyReturnList.pop(0)
+            #take a reading
+            KEY1 = GPIO.input(4)
+            KEY2 = GPIO.input(14)
+            KEY3 = GPIO.input(15)
+            KEY4 = GPIO.input(17)
+            KEY5 = GPIO.input(18)
+            #if the last reading was low and this one high, alert us
+            if ((not prev_KEY1) and KEY1):
+                print("Played: C5")
+                keyReturnList.append("C5")
+            if ((not prev_KEY2) and KEY2):
+                print("Played: D5")
+                keyReturnList.append("D5")
+            if ((not prev_KEY3) and KEY3):
+                print("Played: E5")
+                keyReturnList.append("E5")
+            if ((not prev_KEY4) and KEY4):
+                print("Played: F5")
+                keyReturnList.append("F5")
+            if ((not prev_KEY5) and KEY5):
+                print("Played: G5")
+                keyReturnList.append("G5")
+            #update previous input
+            prev_input = KEY1
+            #slight pause
+            time.sleep(0.10)
--- pianomain.py
+++ pianomain.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 from pianoapp import PianoApp
+import RPi.GPIO as GPIO
 
 def main():
     app = PianoApp()
     app.run()
+    GPIO.cleanup()
 
 if __name__ == "__main__": main()
