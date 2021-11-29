2e586391ec0f2e2226f2430e9ef863c86cef7c77
Zlakesyx <jdcoffey54@gmail.com>
2021-11-28 22:53:18 -0800

    keyboard testing

--- keyboard.py
+++ keyboard.py
@@ -7,6 +7,7 @@ from time import sleep
 
 class Keyboard:
     KEY_COLORS = {'C5': 'green', 'D5': 'red', 'E5': 'orange', 'F5': 'pink', 'G5': 'purple'}
+    RETURN_SEQUENCE = ['C5', 'C5', 'C5', 'C5', 'C5']
     keyPlayed = None
 
     GPIO.setmode(GPIO.BCM)
@@ -15,18 +16,9 @@ class Keyboard:
     GPIO.setup(15,GPIO.IN)
     GPIO.setup(17,GPIO.IN)
     GPIO.setup(18,GPIO.IN)
-    
-    @staticmethod
-    def playKeyWithLight(key):
-        keyPlayed = key 
-
-    @staticmethod
-    def resetKey():
-        keyPlayed = None
 
     @staticmethod
     def play(controller):
-        returnSequence = ['C5', 'C5', 'C5', 'C5', 'C5']
         keyReturnList = []
         #initialise a previous input variable to 0 (Assume no pressure applied)
         prev_KEY1 = 0
@@ -37,8 +29,60 @@ class Keyboard:
         while True:
             print(keyReturnList)
             if (len(keyReturnList) == 5):
-                print(keyReturnList)
-                if (keyReturnList == returnSequence):
+                if (keyReturnList == Keyboard.RETURN_SEQUENCE):
+                    break;
+            if (len(keyReturnList) > 5):
+                keyReturnList.pop(0)
+            #take a reading
+            KEY1 = GPIO.input(4)
+            KEY2 = GPIO.input(14)
+            KEY3 = GPIO.input(15)
+            KEY4 = GPIO.input(17)
+            KEY5 = GPIO.input(18)
+            #if the last reading was low and this one high, alert us
+            if ((not prev_KEY1) and KEY1):
+                print("Played: C5")
+                keyReturnList.append("C5")
+                Keyboard.keyPlayed = "C5"
+            elif ((not prev_KEY2) and KEY2):
+                print("Played: D5")
+                keyReturnList.append("D5")
+                Keyboard.keyPlayed = "D5"
+            elif ((not prev_KEY3) and KEY3):
+                print("Played: E5")
+                keyReturnList.append("E5")
+                Keyboard.keyPlayed = "E5"
+            elif ((not prev_KEY4) and KEY4):
+                print("Played: F5")
+                keyReturnList.append("F5")
+                Keyboard.keyPlayed = "F5"
+            elif ((not prev_KEY5) and KEY5):
+                print("Played: G5")
+                keyReturnList.append("G5")
+                Keyboard.keyPlayed = "G5"
+
+            controller.playKeyWithLight(Keyboard.keyPlayed, 0.1)
+            #update previous input
+            prev_KEY1 = KEY1
+            prev_KEY2 = KEY2
+            prev_KEY3 = KEY3
+            prev_KEY4 = KEY4
+            prev_KEY5 = KEY5
+            controller.update()
+
+    @staticmethod
+    def learn(controller):
+        keyReturnList = []
+        #initialise a previous input variable to 0 (Assume no pressure applied)
+        prev_KEY1 = 0
+        prev_KEY2 = 0
+        prev_KEY3 = 0
+        prev_KEY4 = 0
+        prev_KEY5 = 0
+        while True:
+            print(keyReturnList)
+            if (len(keyReturnList) == 5):
+                if (keyReturnList == Keyboard.RETURN_SEQUENCE):
                     break;
             if (len(keyReturnList) > 5):
                 keyReturnList.pop(0)
@@ -53,19 +97,19 @@ class Keyboard:
                 print("Played: C5")
                 keyReturnList.append("C5")
                 controller.playKeyWithLight("C5", 0.1)
-            if ((not prev_KEY2) and KEY2):
+            elif ((not prev_KEY2) and KEY2):
                 print("Played: D5")
                 keyReturnList.append("D5")
                 controller.playKeyWithLight("D5", 0.1)
-            if ((not prev_KEY3) and KEY3):
+            elif ((not prev_KEY3) and KEY3):
                 print("Played: E5")
                 keyReturnList.append("E5")
                 controller.playKeyWithLight("E5", 0.1)
-            if ((not prev_KEY4) and KEY4):
+            elif ((not prev_KEY4) and KEY4):
                 print("Played: F5")
                 keyReturnList.append("F5")
                 controller.playKeyWithLight("F5", 0.1)
-            if ((not prev_KEY5) and KEY5):
+            elif ((not prev_KEY5) and KEY5):
                 print("Played: G5")
                 keyReturnList.append("G5")
                 controller.playKeyWithLight("G5", 0.1)
